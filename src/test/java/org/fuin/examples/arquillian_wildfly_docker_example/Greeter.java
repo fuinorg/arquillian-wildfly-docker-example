b9ff2dc516ffb50482f759de391d4d6e9cee3ea2
Michael Schnell <michael.schnell@lhind.dlh.de>
2019-04-21 19:44:15 +0200

    Initial commit

---
+++ src/test/java/org/fuin/examples/arquillian_wildfly_docker_example/Greeter.java
@@ -0,0 +1,9 @@
+package org.fuin.examples.arquillian_wildfly_docker_example;
+
+public class Greeter {
+
+    public String createGreeting(String name) {
+        return "Hello, " + name + "!";
+    }
+
+}
